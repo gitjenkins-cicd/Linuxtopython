3b4b10ab6861a25a5b9e604a4cb1651ffb34b8cd
root <root@localhost.localdomain>
2021-06-14 10:31:32 -0700

    Functions added

--- First1.py
+++ First1.py
@@ -1,2 +1,27 @@
 import subprocess
 subprocess.call(["ls","-lrth","/tmp/"])
+
+dickspace="df"
+diskspacearg="-h"
+#subprocess.call([dickspace,diskspacearg])
+
+subprocess.call("df -h",shell=True)
+#subprocess.call("find / -type f -exec ls -lrth {} \;",shell=True)
+
+def disk_func():
+    dickspace="df"
+    diskspacearg="-h"
+    subprocess.call([dickspace,diskspacearg])
+
+def uname_func():
+    uname = "uname"
+    uname_arg = "-a"
+    subprocess.call([uname, uname_arg])
+
+
+def main():
+    uname_func()
+    disk_func()
+
+print("Printinng from main function")
+main()
